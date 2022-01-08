f4007262999f42eb3beead86ca4d4e748b02ef2d
mklavuhn <michaelklavuhn3@gmail.com>
2022-01-07 16:30:18 -0800

    commit tree 6d95cb465e9643682fe48215b4879be2ecce9dab

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.collect do |t|
+            firstname = t.first_name.to_s 
+            lastname = t.last_name.to_s
+            "#{firstname} #{lastname}"
+        end
+    end
+
 end
